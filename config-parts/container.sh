e31b1a57d376b6bb704562958b5af630fb7aae52
Bernd Schorgers <me@bjw-s.dev>
2023-02-25 16:39:23 +0100

    feat: Replace built-in ddns with container

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -3,6 +3,19 @@
 # Container networks
 set container network services prefix '10.5.0.0/24'
 
+# cloudflare-ddns
+set container name cloudflare-ddns allow-host-networks
+set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
+set container name cloudflare-ddns environment DOMAINS value 'ipv4.schorgers.nl,ipv4.bjw-s.tech,ipv4.bjw-s.dev,ipv4.bjws.nl'
+set container name cloudflare-ddns environment IP6_PROVIDER value "none"
+set container name cloudflare-ddns environment TZ value 'Europe/Amsterdam'
+set container name cloudflare-ddns environment PGID value "1000"
+set container name cloudflare-ddns environment PUID value "1000"
+set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.8.3'
+set container name cloudflare-ddns memory '0'
+set container name cloudflare-ddns restart 'on-failure'
+set container name cloudflare-ddns shared-memory '0'
+
 # coredns - main instance
 set container name coredns cap-add 'net-bind-service'
 set container name coredns image 'docker.io/coredns/coredns:1.10.1'
