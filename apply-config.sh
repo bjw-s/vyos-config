3a8d142c6f6244fd6ee429e74d6a74427a198d08
Bernd Schorgers <me@bjw-s.dev>
2023-03-12 12:27:32 +0100

    feat: Directly pull images in apply-config.sh

--- apply-config.sh
+++ apply-config.sh
@@ -1,8 +1,10 @@
 #!/bin/vbash
+# shellcheck shell=bash
+# shellcheck source=/dev/null
 dry_run=false
 
 if [ "$(id -g -n)" != 'vyattacfg' ] ; then
-    exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
+    exec sg vyattacfg -c "/bin/vbash $(readlink -f "$0") $@"
 fi
 
 while getopts "d" options; do
@@ -50,6 +52,18 @@ if "$dry_run"; then
   # Show what's different from the running config
   compare
 else
+  # Pull container images
+  AVAILABLE_IMAGES=($(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }'))
+  CONFIG_IMAGES=($(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq))
+
+  for image in "${CONFIG_IMAGES[@]}"
+  do
+    if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ " ${image} " ]]; then
+      echo "Pulling image ${image}"
+      run add container image "${image}"
+    fi
+  done
+
   # Commit and save
   echo "Committing and saving config"
   commit
