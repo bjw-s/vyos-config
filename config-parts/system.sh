1fb59db2d999d2da654bf106f965c88a7e027d04
Bernd Schorgers <me@bjw-s.dev>
2023-05-13 09:50:41 +0200

    fix: Update syslog format config

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -17,6 +17,7 @@ set system sysctl parameter kernel.pty.max value '24000'
 set system syslog host 10.45.0.2 facility kern level 'warning'
 set system syslog host 10.45.0.2 protocol 'tcp'
 set system syslog host 10.45.0.2 port '6001'
+set system syslog host 10.45.0.2 format 'octet-counted'
 
 set system task-scheduler task backup-config crontab-spec '30 0 * * *'
 set system task-scheduler task backup-config executable path '/config/scripts/custom-config-backup.sh'
