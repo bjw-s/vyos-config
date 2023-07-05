0849208c8977297b91a52acb2d080bd3b08e2caa
budimanjojo <budimanjojo@gmail.com>
2023-07-05 12:31:12 +0700

    feat: shellcheck recommended code

--- apply-config.sh
+++ apply-config.sh
@@ -4,7 +4,7 @@
 dry_run=true
 
 if [ "$(id -g -n)" != 'vyattacfg' ] ; then
-    exec sg vyattacfg -c "/bin/vbash $(readlink -f "$0") $@"
+    exec sg vyattacfg -c "/bin/vbash $(readlink -f "$0") $*"
 fi
 
 while getopts "c" options; do
@@ -37,26 +37,27 @@ fi
 
 # Apply environment to container (configuration) files
 restart_containers=""
-for file in $(find containers -type f -name "*.tmpl"); do
+while IFS= read -r -d '' file
+do
   cfgfile="${file%.tmpl}"
 
   shafile=$file.sha256
-  if ! test -e $shafile; then
-    echo "rebuild" >$shafile
+  if ! test -e "$shafile"; then
+    echo "rebuild" >"$shafile"
   fi
 
-  newsha=$(envsubst <$file | shasum -a 256 | awk '{print $1}')
-  oldsha=$(cat $shafile)
+  newsha=$(envsubst <"$file" | shasum -a 256 | awk '{print $1}')
+  oldsha=$(cat "$shafile")
 
-  if ! test $newsha == $oldsha; then
+  if ! test "$newsha" == "$oldsha"; then
     echo "Configuration changed for $file"
     if ! "$dry_run"; then
       envsubst <"$file" >"$cfgfile"
-      echo $newsha >$shafile
-      restart_containers="$restart_containers $(echo $file | awk -F / '{print $1}')"
+      echo "$newsha" >"$shafile"
+      restart_containers="$restart_containers $(echo "$file" | awk -F / '{print $1}')"
     fi
   fi
-done
+done < <(find containers -type f -name "*.tmpl" -print0)
 
 # Include VyOS specific functions and aliases
 source /opt/vyatta/etc/functions/script-template
@@ -77,11 +78,11 @@ if "$dry_run"; then
   compare
 else
   # Pull new container images
-  AVAILABLE_IMAGES=($(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }'))
-  CONFIG_IMAGES=($(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq))
+  mapfile -t AVAILABLE_IMAGES < <(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }')
+  mapfile -t CONFIG_IMAGES < <(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq)
 
   for image in "${CONFIG_IMAGES[@]}"; do
-    if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ " ${image} " ]]; then
+    if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ \ ${image}\  ]]; then
       echo "Pulling image ${image}"
       run add container image "${image}"
     fi
@@ -100,7 +101,7 @@ else
     image_id=$(echo "${image}" | awk '{ print $3 }')
     image_name_tag="${image_name}:${image_tag}"
 
-    if [[ ! " ${CONFIG_IMAGES[*]} " =~ " ${image_name_tag} " ]]; then
+    if [[ ! " ${CONFIG_IMAGES[*]} " =~ \ ${image_name_tag}\  ]]; then
       echo "Removing container ${image_name_tag}"
       run delete container image "${image_id}"
     fi
@@ -108,7 +109,7 @@ else
 
   # Restart containers
   for container in $restart_containers; do
-    run restart container $container
+    run restart container "$container"
   done
 fi
 
